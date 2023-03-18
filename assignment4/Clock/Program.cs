6a7858080e003e6f0341a66303797e97af1a8761
DYX <715373873@qq.com>
2023-03-18 16:34:09 +0800

    assignment4

---
+++ assignment4/Clock/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Clock
+{
+    public delegate void TickHandler(object sender, EventArgs e);
+    public delegate void AlarmHandler(object sender, EventArgs e);
+
+    class Clock
+    {
+        public event TickHandler? Tick;
+        public event AlarmHandler? Alarm;
+
+        private int hour = DateTime.Now.Hour;
+        private int minute= DateTime.Now.Minute;
+        private int second= DateTime.Now.Second;
+
+        EventArgs args = new EventArgs();
+        public void Start()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000); // 让线程休眠1秒钟，模拟时间的流逝
+
+                OnTick();
+                hour = DateTime.Now.Hour;
+                minute = DateTime.Now.Minute;
+                second = DateTime.Now.Second;
+               
+                if (DateTime.Now.Second == 0) //每分钟刚开始时触发Alarm
+                {
+                    OnAlarm(); 
+                }
+            }
+        }
+        public void GetTime()
+        {
+            Console.WriteLine($"当前时间为：{hour}:{minute}:{second}");
+        }
+
+        public void OnTick()
+        {
+            Tick(this, args);
+        }
+
+        public void OnAlarm()
+        {
+            Alarm(this, args);
+        }
+    }
+
+    class Program
+    {
+        static void Tick(object sender, EventArgs e)
+        {
+            Console.WriteLine("Tick");
+        }
+
+        static void Alarm(object sender, EventArgs e)
+        {
+            Console.WriteLine("Alarm!");
+        }
+
+        static void Main(string[] args)
+        {
+            Clock myClock = new Clock();
+
+            myClock.Tick += Tick;
+            myClock.Alarm += Alarm;
+            myClock.GetTime();
+            myClock.Start();
+        }
+    }
+}
---
+++ assignment4/Link/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace project1
+{
+    public class Node<T>
+    {
+        public Node<T>? Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T data)
+        { 
+            this.Data = data;
+            Next = null;
+        }
+
+    }
+
+    public class List<T>
+    {
+        private Node<T>?head;
+        private Node<T>?tail;
+
+        public List()
+        {
+            head =tail= null;
+        }
+        public Node<T>? Head
+        {
+            get { return head; }
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (head == null)
+            {
+                head = tail= n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            Node<T>?p = head;
+            while (p != null)
+            {
+                action(p.Data);
+                p = p.Next;
+            }
+            
+        }
+    }
+    class Test
+    {
+        static void Main(string[]args)
+        {
+            //创建double型list
+            List<double> list = new List<double>();
+            for(double i=1;i<=10;i++)
+            {
+                list.Add(i);
+            }
+
+            //打印链表
+            Console.WriteLine("链表中的元素为:");
+            list.ForEach(m => Console.Write(m +" ")) ;
+            Console.WriteLine();
+
+            //求最大值
+            double max = Double.MinValue;
+            Action<double> GetMax = (m =>
+            {
+                if (max < m) { max = m; }
+            });
+            list.ForEach(GetMax);
+            Console.WriteLine("链表中最大值为:" + max);
+
+            //求最小值
+            double min = Double.MaxValue;
+            Action<double> GetMin = (m =>
+            {
+                if (min > m) { min = m; }
+            });
+            list.ForEach(GetMin);
+            Console.WriteLine("链表中最小值为:"+min);
+
+            //求和
+            double sum = 0;
+            list.ForEach(i => sum += i);
+            Console.WriteLine("链表中元素之和为:"+sum);
+        }
+    }
+}
